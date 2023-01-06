35098997590024b46b38f94e8516e3c1ae22d0ad
Cenk Şengünlü <cenksengunlu@hotmail.com>
2023-01-06 23:36:55 +0300

    Update SubmitButton.tsx

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -5,7 +5,7 @@ const SubmitButton = ({ canSave }: { canSave: boolean }) => {
     <button
       type="submit"
       disabled={!canSave}
-      className="mt-5 border-2 text-lg border-black py-2 px-4 cursor-pointer transition ease-in-out hover:bg-orange-500 hover:border-orange-500 hover:text-white duration-500 disabled:cursor-not-allowed disabled:opacity-50 rounded-lg"
+      className="mt-5 border-2 text-lg border-black py-2 px-4 cursor-pointer transition ease-in-out duration-500 hover:bg-orange-500 hover:border-orange-500 hover:text-white disabled:cursor-not-allowed disabled:opacity-50 rounded-lg"
     >
       Kaydet
     </button>
