aa82e4fffd32766fb1de52d8a786fd34dbd32819
Cenk Şengünlü <cenksengunlu@hotmail.com>
2023-01-17 21:16:52 +0300

    edited bus component

--- components/BusComponent.tsx
+++ components/BusComponent.tsx
@@ -23,29 +23,46 @@ const BusComponent = ({
     // use for and loop for seatNumber
     <>
       {Number(seatNumber) !== 0 && Number(seatNumber) % seatRow === 0 && (
-        <div className="border-2 px-5 h-96 w-[65%] py-5 overflow-auto rounded-xl">
-          <div className="w-10 h-10 mb-5 rounded bg-red-500"></div>
-          <div
-            className={`grid ${
-              seatRow === 3 ? "grid-cols-3" : "grid-cols-4"
-            } gap-4`}
-          >
-            {arr.map((seat, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`border-2 w-10 h-10 flex items-center justify-center rounded border-black p-2 cursor-pointer hover:bg-green-300 ${
-                    index % seatRow === 0 ? "bg-blue-300" : ""
-                  } ${selectedSeat === index ? "bg-green-300" : ""}`}
-                  onClick={() => handleSelect(index)}
-                >
-                  {index + 1}
-                </div>
-              );
-            })}
+        <div className="h-full w-[60%] flex items-center">
+          <div className="border-2 p-5 py-15 w-full overflow-auto rounded-xl">
+            <div className="w-10 h-10 mb-5 rounded bg-red-500"></div>
+            <div
+              className={`grid ${
+                seatRow === 3 ? "grid-cols-4" : "grid-cols-5"
+              } gap-2`}
+            >
+              {arr.map((seat, index) => {
+                return (
+                  <>
+                    <div
+                      key={index}
+                      className={`border-2 w-10 h-10 flex items-center justify-center rounded border-black p-2 cursor-pointer hover:bg-green-300 ${
+                        index % seatRow === 0 && seatRow === 3
+                          ? "bg-blue-300"
+                          : index % seatRow === 1 && seatRow === 4
+                          ? "bg-blue-300"
+                          : ""
+                      } ${selectedSeat === index ? "bg-green-300" : ""}`}
+                      onClick={() => handleSelect(index)}
+                    >
+                      {index + 1}
+                    </div>
+                    <>
+                      {index % seatRow === 0 && seatRow === 3 && (
+                        <div className="grow"></div>
+                      )}
+                      {index % seatRow === 1 && seatRow === 4 && (
+                        <div className="grow"></div>
+                      )}
+                    </>
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
       )}
+
       {(Number(seatNumber) === 0 || Number(seatNumber) % seatRow !== 0) &&
         type !== "" && (
           <div>
